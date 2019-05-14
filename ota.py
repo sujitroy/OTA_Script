088b472cca864f98f2001e22700875021c6ef079
Sujit Roy <kumarsujitroy@gmail.com>
2019-05-14 13:34:43 +0530

    changes for X00TD
    Signed-off-by: Sujit Roy <kumarsujitroy@gmail.com>

--- ota.py
+++ ota.py
@@ -14,10 +14,10 @@ def md5(fname):
 # constants
 donate_url = "https://www.paypal.me/rakeshbatra"
 website_url = "https://plus.google.com/communities/111037372581335961267"
-developer = "RakeshBatra"
-developer_url = "https://forum.xda-developers.com/member.php?u=5985430"
-forum_url= "https://forum.xda-developers.com/oneplus-3/oneplus-3--3t-cross-device-development/rom-coltos-t3808635"
-url= "https://sourceforge.net/projects/coltos/files/OP3_3T_Unified/"
+developer = "SujitRoy"
+developer_url = "https://forum.xda-developers.com/member.php?u=7598073"
+forum_url= "https://forum.xda-developers.com/asus-zenfone-max-pro-m1/development/rom-colt-enigma-t3864709"
+url= "https://sourceforge.net/projects/coltos/files/X00TD/"
 device = sys.argv[1]
 filename = "ColtOS-Enigma-4.1_OFFICIAL-" + time.strftime('%Y%m%d') + "-" + device + ".zip"
 zip_path = os.path.expanduser("~") + "/colt/out/target/product/" + device + "/"
